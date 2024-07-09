e046f4869404818299e33fa1ddaeb9a27a7f7b39
Sai Achuth <35956262+saiachuth@users.noreply.github.com>
2024-07-09 15:27:12 +0530

    Create config.py
    added the config file for better connection on local run

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    MYSQL_HOST = 'localhost' #enter your local host or url
+    MYSQL_USER = 'root' #update user details as per your usage (default: root)
+    MYSQL_PASSWORD = 'your password' #enter the password for your local connection 
+    MYSQL_DB = 'post_comments_db' #enter your schema name 
+    MYSQL_CURSORCLASS = 'DictCursor'
